d510f3678bb82e1f5c412bd5fed02ff9f690066a
Arnabal <144528507+Arnaball@users.noreply.github.com>
2026-03-31 20:25:45 +0200

    primer componente img (devJobsAvatar)

---
+++ JS/devjobs-avatar-element.js
@@ -0,0 +1,35 @@
+class DevJobsAvatar extends HTMLElement {
+  constructor() {
+    super(); // llamar al constructor de HTMLElement para que podamos utilzarlo
+    this.attachShadow({ mode: "open" }); // Encapsula nuestro componente para que no se vea afectado por estilos externos y viceversa
+  }
+
+  render() {
+    const service = this.getAttribute("service") ?? "github"; // Si es nulo o undefined utiliza por defecto lo que hay detrás de las ??
+    const name = this.getAttribute("name") ?? "midudev";
+    const size = this.getAttribute("size") ?? "40";
+
+    const URL = this.createURL(service, name);
+
+    console.log({ service, name, size });
+    this.shadowRoot.innerHTML = `
+        <img
+        src = ${URL}
+        alt="Imagen de perfil"
+        class="avatar"
+        style="width: ${size}px; height: ${size}px; border-radius: 50%;"
+      />
+    `;
+  }
+
+  createURL(service, name) {
+    return `"https://unavatar.io/${service}/${name}"`;
+  }
+
+  // Esto hace que cuando el componente aparezca en el ordenador  renderice lo de arriba
+  connectedCallback() {
+    this.render();
+  }
+}
+
+customElements.define("dev-jobs-avatar", DevJobsAvatar);
--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,4 @@
 import "./fetch-data.js";
 import "./apply-button.js";
 import "./filters.js";
+import "./devjobs-avatar-element.js";
