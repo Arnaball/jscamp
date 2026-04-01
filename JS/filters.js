d17a01098214a1e066ffc7a65355aa38cf774e37
Arnabal <144528507+Arnaball@users.noreply.github.com>
2026-04-01 10:43:53 +0200

    optimizacion de input search

--- JS/filters.js
+++ JS/filters.js
@@ -1,23 +1,6 @@
 const filtros = document.querySelectorAll(".filter-select");
 const resultadoBusquedaTextP = document.querySelector("#resultado-busqueda");
 const inputSearch = document.querySelector("#search-input");
-const jobCards = document.querySelector(".jobLIsting");
-
-let jobsTitles = [];
-let jobsArticles = [];
-
-function getJobs() {
-  jobsArticles = [];
-  jobsTitles = [];
-
-  const jobArticles = document.querySelectorAll(".job-listing-card");
-
-  jobArticles.forEach((jobArticle) => {
-    const jobsH3 = jobArticle.querySelector("div > h3");
-    jobsTitles.push(jobsH3.textContent);
-    jobsArticles.push(jobArticle);
-  });
-}
 
 filtros.forEach((filtro) => {
   filtro.addEventListener("change", (e) => {
@@ -40,18 +23,12 @@ filtros.forEach((filtro) => {
   });
 });
 
-inputSearch.addEventListener("focus", () => {
-  getJobs();
-});
-
 inputSearch.addEventListener("input", (e) => {
   let valorIndicado = e.target.value;
 
-  jobsTitles.forEach((jobTitle) => {
-    jobsArticles.forEach((jobArticle) => {
-      console.log(jobTitle);
-      const seMuestra = jobTitle.includes(valorIndicado);
-      jobArticle.classList.toggle("is-hidden", !seMuestra);
-    });
+  document.querySelectorAll(".job-listing-card").forEach((job) => {
+    let h3Job = job.querySelector("div > h3");
+    const seMuestra = h3Job.textContent.includes(valorIndicado);
+    job.classList.toggle("is-hidden", !seMuestra);
   });
 });
