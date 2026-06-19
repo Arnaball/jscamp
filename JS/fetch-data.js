98fd538f4f730716acdb9d2b48162cca3c03015e
Arnabal <144528507+Arnaball@users.noreply.github.com>
2026-06-19 15:34:30 +0200

    Estructura inicial json

--- JS/fetch-data.js
+++ JS/fetch-data.js
@@ -1,14 +1,41 @@
 const jobListingContainer = document.querySelector(".job-listing");
+const paginationNav = document.querySelector(".pagination");
+const RESULTS_PER_PAGE = 3;
+const currentPage = 1;
+// <a href="" class="is-active">1</a>
 
 fetch("./json/data.json") // El fetch es asíncrono
-  .then((res) => {
-    return res.json();
-  })
+  .then((res) => res.json())
   .then((jobs) => {
-    jobs.forEach((job) => {
-      buildJobArticle(job);
-    });
+    let pages = Math.ceil(jobs.length / RESULTS_PER_PAGE);
+    buildPages(pages);
+    renderJobs(jobs, currentPage);
+  })
+  .catch((error) => {
+    console.error("Error cargando empleos:", error);
   });
+
+function buildPages(pages) {
+  for (let i = 1; i < pages + 1; i++) {
+    const pagination = document.createElement("a");
+
+    pagination.textContent = i;
+    paginationNav.appendChild(pagination);
+
+    if (i === currentPage) {
+      pagination.classList.add("is-active");
+    }
+  }
+}
+
+function renderJobs(jobs, page) {
+  const startIndex = (page - 1) * RESULTS_PER_PAGE;
+  const pagedJobs = jobs.slice(startIndex, startIndex + RESULTS_PER_PAGE);
+
+  pagedJobs.forEach((job) => {
+    buildJobArticle(job);
+  });
+}
 
 function buildJobArticle(job) {
   const article = document.createElement("article");
